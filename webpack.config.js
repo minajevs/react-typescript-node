4cb2e19cbdf3ebb3714f6f61093ffc1edcd07a56
dexiesharp <d3x133@gmail.com>
2016-12-07 10:17:05 +0200

    fixed auto build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 var glob = require("glob");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         js: glob.sync("./app/web/js/*.js")
-    },  
+    },
     output: {
         path: __dirname + "/app/web",
         filename: "bundle.js"
     },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: __dirname + "/src/web",
+                to: __dirname + "/app/web"
+            }
+        ], {
+            ignore: [ "*.js", "*.ts", "*.jsx", "*.tsx" ]    
+        })
+    ],
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" }
